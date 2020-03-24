10bb82a528990e6bbe43d102b39da72f379915d6
Jocelyn <59553647+JocelynMilRodriguez@users.noreply.github.com>
2020-03-24 03:19:49 -0400

    Add files via upload

---
+++ JocelynInitials.swift
@@ -0,0 +1,24 @@
+// I love Tacos and Cesear Salads! 
+
+
+print("  J J J J J")   
+print("      J    ")  
+print("      J    ") 
+print("      J    ")  
+print(" J    J    ") 
+print(" J    J    ")  
+print("   J J    ")   
+print("            ")                     
+print("R R R R R R")
+print("R   *   * R") 
+print("R    >    R")
+print("R  ______ R")
+print("R R R  R  R")
+print("R      R")
+print("R        R")
+print("R        R")
+print("R        R")
+
+
+
+
---
+++ Jocelyninitials2.swift
@@ -0,0 +1,51 @@
+//Jocelyn M. Rodriguez
+//March 23rd 2020
+//Input: None
+//Output:
+//Purpose: *Update on JocelynIntials* - Create a Swift program called JMRInitials.swift that displays the initials of my name in block letters.
+
+
+
+let lines: [String] = [  // Create an array for the characters
+    "  J J J J J",
+    "      J    ",
+    "      J    ",
+    "      J    ",
+    " J    J    ",
+    " J    J    ",
+    "   J J     ",
+    "           ",
+    "R R R R R R",
+    "R   *   * R",
+    "R    >    R",
+    "R  ______ R",
+    "R R R  R  R",
+    "R      R   ",
+    "R        R ",
+    "R        R ",
+    "R        R ",
+
+    "  J J J J J",
+    "      J    ",
+    "      J    ",
+    "      J    ",
+    " J    J    ",
+    " J    J    ",
+    "   J J     ",
+    "           ",
+    "R R R R R R",
+    "R   *   * R",
+    "R    >    R",
+    "R  ______ R",
+    "R R R  R  R",
+    "R      R   ",
+    "R        R ",
+    "R        R ",
+    "R        R "
+]
+
+for line in lines { // For-In Loop to iterate my string of characters
+    print(line)// Print the sequence named 'Lines'
+}
+
+
